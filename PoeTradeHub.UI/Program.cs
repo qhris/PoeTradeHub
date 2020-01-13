7a208d3a67775bd36595899c739661b51171ee43
Christoffer Hjalmarsson <b.christoffer.hjalmarsson@gmail.com>
2020-01-13 17:55:58 +0100

    Only allow for one instance of the application to run

---
+++ PoeTradeHub.UI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace PoeTradeHub.UI
+{
+    public static class Program
+    {
+        private const string ApplicationGuid = "E2706AEA-744F-401F-B868-33C423ABE531";
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using (var mutex = new Mutex(false, $@"Global\{ApplicationGuid}"))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Another instance of the application is already running.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
